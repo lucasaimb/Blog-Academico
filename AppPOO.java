043292667229633a380691b3ef5f74052db62efe
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2022-10-19 22:38:43 -0300

    Update AppPOO.java

--- AppPOO.java
+++ AppPOO.java
@@ -5,15 +5,18 @@ import java.util.*;
 public class AppPOO {
 
     public static void main(String[] args) {
-        Aluno a1 = new Aluno("biologia marinha", "lucas", "lucasss", 2202258, 2323);
-        Professor p1 = new Professor("Mario", "mariozinho", 232323, 309307);
-        Coordenador c1 =  new Coordenador("Susilea", "susi0102", 323223, 432332);
+        
+       // ArrayList<Trabalho> trabalhosPostados = new ArrayList<Trabalho>();
+        
+        Aluno a1 = new Aluno("Lucas Imbroisi", "SI2N", 2202258);
+        Professor p1 = new Professor("Maria", 3941);
+        Coordenador c1 =  new Coordenador("Susilea", 332, "SistemasdeInformação");
         Trabalho t1 = new Trabalho(2333, "biologia", a1, "lorem ipsum", "Amazonia e o ecossistema");
-        a1.curtir(t1);
-        p1.curtir(t1);
-        c1.curtir(t1);
-        a1.visualizarTrabalho(t1);
-        a1.download(t1);
+        a1.Curtir(t1);
+        p1.Curtir(t1);
+        c1.Curtir(t1);
+        a1.VisualizarTrabalho(t1);
+        a1.Download(t1);
        
         
     }
