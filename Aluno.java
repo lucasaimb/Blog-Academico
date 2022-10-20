42fc85af06d8c78c0ce6596ed9a980eb5fd9d226
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2022-10-19 22:37:45 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,58 +1,26 @@
 package apppoo;
 
-public class Aluno{ 
-    public String nome, user, curso;
-    int matricula, senha;
-    public Aluno(String curso, String nome, String user, int matricula, int senha){
+public class Aluno extends Usuario{ 
+    public String curso;
+
+    public Trabalho t;
+    
+    
+    public Aluno() {
+    }
+    
+    
+    
+    public Aluno(String nome, String curso, int matricula){
         this.nome = nome;
-        this.user = user;
         this.matricula = matricula;
-        this.senha = senha;
         this.curso = curso;       
     }
     
-  
-    public void curtir(Trabalho qualTrabalho){
-       qualTrabalho.curtidas = qualTrabalho.curtidas + 1;
-    }
-    
-    public void comentar(){
+    void CriarTrabalho(){
         
+        t = new Trabalho();
     }
-    
-    public void visualizarTrabalho(Trabalho trab){
-        
-        System.out.println("Titulo: "+trab.titulo);
-        System.out.println("Autor: "+trab.aluno.getNome());
-        System.out.println("Area: "+trab.getAreaAtuacao());
-        System.out.println("Trabalho: "+trab.getConteudoTrabalho());
-        System.out.println("Curtidas: "+trab.getCurtidas());
-        System.out.println("Comentarios: "+trab.getComentarios());
-    }
-    
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    
-    
-    public void setMatricula(int matricula){
-        this.matricula = matricula;
-    }
-
-    public int getMatricula() {
-        return matricula;
-    }
-    
-
-    
-    public void download(Trabalho trab){
-        System.out.println("Voce executou o download do trabalho "+'"'+trab.titulo+'"'+".");
-    }
-    
     
     
 }
