355bfea2e59050ee67d9e5c1ec70537a48330102
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2022-10-20 21:11:28 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,3 +1,5 @@
+package app;
+
 import java.util.ArrayList;
 
 public class Professor extends Usuario{
@@ -5,36 +7,42 @@ public class Professor extends Usuario{
     Trabalho trabalhoParaPostar;
     ArrayList<Trabalho> trabalhosJaPostados;
 
+    // Construtor padrao
     public Professor() {
+    
     }
   
-    
-    
+    // Construtor completo
     public Professor(String nome, int matricula) {
         this.nome = nome;
         this.matricula = matricula;
     }
     
+    // Metodo para o professor postar um trabalho
     public void PostarTrabalho(Trabalho trabalho, ArrayList<Trabalho> trabalhosJaPostados){
 
         trabalhosJaPostados.add(trabalho);
     }
     
+    // Metodo para o professor listar todos os trabalhos
     public void ListarTrabalhosPostados(ArrayList<Trabalho> listaTrabalhos){
 
         for (Trabalho trabalho : listaTrabalhos) {
             
             System.out.println("Dados do trabalho postado:");
+             System.out.println("---------------------------");
             System.out.println("Id: " + trabalho.id);
-            System.out.println("Título: " + trabalho.titulo);
-            System.out.println("Aluno responsável: " + trabalho.aluno.nome);
-            System.out.println("Área de atuação: " + trabalho.areaAtuacao);
-            System.out.println("Conteúdo: " + trabalho.conteudoTrabalho);
+            System.out.println("Titulo: " + trabalho.titulo);
+            System.out.println("Aluno responsavel: " + trabalho.aluno.nome);
+            System.out.println("Area de atuacao: " + trabalho.areaAtuacao);
+            System.out.println("Conteudo: " + trabalho.conteudoTrabalho);
             System.out.println("Curtidas: " + trabalho.curtidas);
             System.out.println("Downloads: " + trabalho.downloads);
+             System.out.println("===========================");
         }
     }
     
+    // Metodo para o professor editar algum trabalho
     public void editarTrabalho(Trabalho qualTrabalho, int id, String areaAtuacao, Aluno aluno, String conteudoTrabalho, String titulo){
         qualTrabalho.id = id;
         qualTrabalho.areaAtuacao = areaAtuacao;
