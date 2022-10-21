59aa2f5ef3dda2971993202e9f8a9d7128214817
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2022-10-20 21:09:37 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,20 +1,23 @@
+package app;
+
 public class Aluno extends Usuario{ 
     
     public String curso;
     public Trabalho trabalho;
     
-    
+    // Construtor padrao
     public Aluno() {
     }
     
     
-    
+    // Construtor completo
     public Aluno(String nome, String curso, int matricula){
         this.nome = nome;
         this.matricula = matricula;
         this.curso = curso;       
     }
     
+    // Metodo para o aluno instanciar um trabalho
     void CriarTrabalho(int id, String area, String conteudo, String titulo){
       
         trabalho = new Trabalho(id, area, this, conteudo, titulo);
