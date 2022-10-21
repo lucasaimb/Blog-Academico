8ac2f52a2f8e64fb1450062e2b5e92a655d79d93
Lucas André Imbroisi <103373950+lucasaimb@users.noreply.github.com>
2022-10-20 21:11:01 -0300

    Update Coordenador.java

--- Coordenador.java
+++ Coordenador.java
@@ -1,23 +1,31 @@
+package app;
+
 public class Coordenador extends Professor{
     public String curso;
 
+    // Construtor padrao
     public Coordenador() {
+        
     }
     
-
+    // Construtor completo
     public Coordenador(String nome, int matricula, String curso) {
         this.nome = nome;
         this.matricula = matricula;
         this.curso = curso;
     }
     
-    public void bloquearAluno(){
+    // Metodo que remove um aluno
+    public void RemoverAluno(Aluno fulano){
         
-        
+        fulano = null;
+        System.gc();
     }
     
-    public void removerComentario(){
+    // Remove os comentarios de um trabalho
+    public void RemoverComentariosDeTrabalho(Trabalho trabalho){
         
+        trabalho.comentarios.clear();
     }
     
 }
